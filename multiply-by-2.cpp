ac650266dc888e10a2ca241facca127fc76bbdf5
elly <lhvo2112@gmail.com>
2025-07-04 17:20:59 +0300

    initial commit

---
+++ multiply-by-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int num{};
+
+  std::cout << "Enter an integer: ";
+  std::cin >> num;
+
+  std::cout << "Double " << num << " is: " << num * 2 << '\n';
+
+  std::cout << "Triple " << num << " is: " << num * 3 << '\n';
+
+  return 0;
+}
